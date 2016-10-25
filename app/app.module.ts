899b513d0eecf3704ad43ad93d70fac56fee0e27
Kyle Pontius <kbpontius@xactware.com>
2016-10-25 15:49:07 -0600

    Finished tutorial.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,28 +1,41 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { HttpModule }     from '@angular/http';
+import './rxjs-extensions';
+
+
+import { AppRoutingModule } from './app-routing.module';
+
+// In-memory API substitutes.
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroService }          from './hero.service';
 import { HeroesComponent }      from './heroes.component';
+import { HeroSearchComponent }  from './hero-search.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HeroService }          from './hero.service';
+
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
+    HeroDetailComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroSearchComponent
   ],
+
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
